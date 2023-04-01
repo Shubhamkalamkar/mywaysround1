29f8bc1a640bc49cf59909ba6f7756659a3fa851
shubham kalamkar <shubhamkalamkar0101@gmail.com>
2023-04-01 15:44:37 +0530

    first commit

--- myways.js
+++ myways.js
@@ -1,29 +1,13 @@
+for( var i=1; i<=31; i++ ) {
 
-
-const fizzbuzz = ()=>{
-    var number = [9,5,5,2,5,3,0,1,0,1]
-    
-    for(i = 0;i<10;i++){
-        var sum
-        if(i==0){
-
-             sum= number[i]
-        }
-        
-       
-        if( (sum%3) == 0 ) {
-        console.log( "Fizz" );
-    }else if( (sum%4) == 0 ) {
-        console.log( "Buzz" );
-     }
-     else if((sum%3) == 0 && (sum%4)==0){
+ if ((i%3) === 0 ) {
+    console.log( "Fizz" );
+}else if( (i%4) === 0 ) {
+    console.log( "Buzz" );
+}
+   else if( (i%3) === 0 && (i%4) === 0 ) {
         console.log( "FizzBuzz" );
-       }else {
-        console.log( sum );
-    }
-    
-    var sum=sum+number[i+1]
+}else{
+        console.log( i );
     }
 }
-
-fizzbuzz()
